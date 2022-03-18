3cd3107675f85cc8345a3fefd7739b10c55efedd
栗田学 <snoopy.mk90@gmail.com>
2022-03-18 15:09:32 +0900

    fix

--- lib/presentation/providers/shop_provider.dart
+++ lib/presentation/providers/shop_provider.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:front/domain/models/shop/shop.dart';
 import 'package:front/domain/repositories/shop_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:openapi/openapi.dart';
+
+// import '../../domain/openapi_factory.dart';
 
 class ShopProvider extends StateNotifier<AsyncValue<Shop>> {
   ShopProvider(this.repository, this.label) : super(const AsyncValue.loading()) {
@@ -15,8 +18,8 @@ class ShopProvider extends StateNotifier<AsyncValue<Shop>> {
   Future<void> _getShop() async {
     try {
       // final Openapi _openApi = OpenApiFactory().build();
-      // final repository = _openApi.getShopsApi();
-      // final response = (await repository.getShops()).data;
+      // final repository = _openApi.getUsersApi();
+      // final response = (await repository.getUsers());
       Shop shop = await repository.getShop(label);
       state = AsyncValue.data(shop);
     } on Exception catch (e) {
